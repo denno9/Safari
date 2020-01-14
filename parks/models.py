f24ac2b87c3d70f2d83c2a90427925eee8385f97
dextersbrain <denno.tz@gmail.com>
2020-01-14 10:57:05 +0300

    third commit

---
+++ parks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class ParksModel(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+    subtitle = models.CharField(max_length=300)
+    description = models.CharField(max_length=5000)
+
+
+
+    def __str__(self):
+        return self.title
